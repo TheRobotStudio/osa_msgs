aeb9b47457d39984efd7312cb52f11848adb81ae
Cyril Jourdan <cyril.jourdan@therobotstudio.com>
2019-07-05 17:36:35 +0200

    v0.1.1

--- src/motor_data_extended.cpp
+++ src/motor_data_extended.cpp
@@ -1,16 +1,20 @@
 /*
- * Copyright (c) 2017, The Robot Studio
- *  All rights reserved.
+ * Copyright (c) 2019, The Robot Studio
+ * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *
- *	* Redistributions of source code must retain the above copyright notice, this
- *	  list of conditions and the following disclaimer.
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
  *
- *	* Redistributions in binary form must reproduce the above copyright notice,
- *	  this list of conditions and the following disclaimer in the documentation
- *	  and/or other materials provided with the distribution.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+
+ * * Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -22,18 +26,15 @@
  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
  */
 
 /**
- * @file masterBoard.cpp
- * @author Cyril Jourdan
- * @date Mar 10, 2017
- * @version 2.0.0
- * @brief Implementation file for the serial communication between ROS and the Master Board
- *
- * Contact: cyril.jourdan@therobotstudio.com
- * Created on : Mar 10, 2017
+ * @file motor_data_extended.cpp
+ * @author Cyril Jourdan <cyril.jourdan.86@gmail.com>
+ * @date Modified on Jul 05, 2019
+ * @date Created on Mar 10, 2017
+ * @version 0.1.1
+ * @brief File for the extended motor data
  */
 
 /*! Includes */
